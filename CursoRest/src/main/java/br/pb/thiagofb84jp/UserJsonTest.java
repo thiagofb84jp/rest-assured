997cf6e75157e6ec934d4baebb5d5d390a0311b8
Thiago Ferreira <thiagobarbosa@EU3146022019005.local>
2023-08-04 09:03:40 -0300

    Including more examples about REST-Assured library

--- CursoRest/src/main/java/br/pb/thiagofb84jp/UserJsonTest.java
+++ CursoRest/src/main/java/br/pb/thiagofb84jp/UserJsonTest.java
@@ -9,6 +9,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Optional;
 
 import static io.restassured.RestAssured.*;
@@ -45,7 +47,7 @@ public class UserJsonTest {
 
         //From
         int id = JsonPath.from(response.asString()).getInt("id");
-        Assert.assertEquals(1, "id");
+        Assert.assertEquals(1, id);
     }
 
     @Test
@@ -90,5 +92,67 @@ public class UserJsonTest {
         .and()
              .body("error", is("Usuário inexistente"))
         ;
+    }
+    
+    @Test
+    public void deveVerificarListaRaiz() {
+        given()
+        .when()
+             .get("http://restapi.wcaquino.me/users/")
+        .then()
+             .statusCode(200)
+        .and()
+             .body("$", hasSize(3))
+             .body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+             .body("age[1]", is(25))
+             .body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+             .body("salary", contains(1234.5678F, 2500, null))
+        ;
+    }
+
+    @Test
+    public void devoFazerVerificacoesAvancadas() {
+        given()
+        .when()
+             .get("http://restapi.wcaquino.me/users/")
+        .then()
+             .statusCode(200)
+        .and()
+             .body("$", hasSize(3))
+             .body("age.findAll{it <= 25}.size()", is(2))
+             .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+             .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+             .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+             .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+             .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+             .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+             .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+             .body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+             .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+             .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()",
+                     allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+             .body("age.collect{it * 2}", hasItems(60, 50, 40))
+             .body("id.max()", is(3))
+             .body("salary.min()", is(1234.5678F))
+             .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678F, 0.001)))
+             .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000D), lessThan(5000D)))
+        ;
+    }
+
+    @Test
+    public void devoUnirJsonPathComJava() {
+        ArrayList<String> names =
+            given()
+            .when()
+                 .get("http://restapi.wcaquino.me/users/")
+            .then()
+                 .statusCode(200)
+            .and()
+                 .extract().path("name.findAll{it.startsWith('Maria')}")
+        ;
+
+        Assert.assertEquals(1, names.size());
+        Assert.assertTrue(names.get(0).equalsIgnoreCase("mArIa Joaquina"));
+        Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());
     }
 }
