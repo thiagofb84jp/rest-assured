2f00570d30438d46be6ac8c57b03efdfb6ed8c98
thiagofb84jp <thiagofb84jp@gmail.com>
2023-08-10 10:53:48 -0300

    Including new files

--- CursoRest/src/main/java/br/pb/thiagofb84jp/VerbosTest.java
+++ CursoRest/src/main/java/br/pb/thiagofb84jp/VerbosTest.java
@@ -3,9 +3,12 @@ package br.pb.thiagofb84jp;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -146,5 +149,113 @@ public class VerbosTest {
         .and()
              .body("error", is("Registro inexistente"))
         ;
+    }
+
+    @Test
+    public void deveSalvarUsuarioUsandoMap() {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("name", "Usuário via map");
+        params.put("age", 25);
+
+        given()
+             .log().all()
+             .contentType(ContentType.JSON)
+             .body(params)
+        .when()
+             .post("http://restapi.wcaquino.me/users")
+        .then()
+             .log().all()
+             .statusCode(201)
+        .and()
+             .body("id", is(notNullValue()))
+             .body("name", is("Usuário via map"))
+             .body("age", is(25))
+        ;
+    }
+
+    @Test
+    public void deveSalvarUsuarioUsandoObjeto() {
+        User user = new User("Usuário via objeto", 25);
+
+        given()
+             .log().all()
+             .contentType(ContentType.JSON)
+             .body(user)
+        .when()
+             .post("http://restapi.wcaquino.me/users")
+        .then()
+             .log().all()
+             .statusCode(201)
+        .and()
+             .body("id", is(notNullValue()))
+             .body("name", is(user.getName()))
+             .body("age", is(user.getAge()))
+        ;
+    }
+
+    @Test
+    public void deveDesserializarObjetoAoSalvarUsuario() {
+        User user = new User("Usuário desserializado", 35);
+
+        User usuarioInserido = given()
+                                    .log().all()
+                                    .contentType(ContentType.JSON)
+                                    .body(user)
+                               .when()
+                                    .post("http://restapi.wcaquino.me/users")
+                               .then()
+                                    .log().all()
+                                    .statusCode(201)
+                                .and()
+                                    .extract().body().as(User.class)
+        ;
+
+        System.out.println(usuarioInserido);
+        Assert.assertThat(usuarioInserido.getId(), notNullValue());
+        Assert.assertEquals("Usuário desserializado", usuarioInserido.getName());
+        Assert.assertThat(usuarioInserido.getAge(), is(usuarioInserido.getAge()));
+    }
+
+    @Test
+    public void deveSalvarUsuarioViaXMLUsandoObjeto() {
+        User user = new User("Usuario XML", 40);
+
+        given()
+             .log().all()
+             .contentType(ContentType.XML)
+             .body(user)
+        .when()
+             .post("http://restapi.wcaquino.me/usersXML")
+        .then()
+             .log().all()
+             .statusCode(201)
+        .and()
+             .body("user.@id", is(notNullValue()))
+             .body("user.name", is(user.getName()))
+             .body("user.age", is(Integer.toString(user.getAge())))
+        ;
+    }
+
+    @Test
+    public void deveDesserializarXMLAoSalvarUsuario() {
+        User user = new User("Usuario XML", 40);
+
+        User usuarioInserido = given()
+                                    .log().all()
+                                    .contentType(ContentType.XML)
+                                    .body(user)
+                               .when()
+                                    .post("http://restapi.wcaquino.me/usersXML")
+                               .then()
+                                    .log().all()
+                                    .statusCode(201)
+                               .and()
+                                    .extract().body().as(User.class)
+        ;
+
+        Assert.assertThat(usuarioInserido.getId(), notNullValue());
+        Assert.assertThat(usuarioInserido.getName(), is(user.getName()));
+        Assert.assertThat(usuarioInserido.getAge(), is(user.getAge()));
+        Assert.assertThat(usuarioInserido.getSalary(), nullValue());
     }
 }
